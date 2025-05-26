962aac1db67dd6c918b9a85cfc94a54855763815
MarkGamboa <202310110@fit.edu.ph>
2025-05-26 17:51:57 +0800

    feat: Add best sellers menu handler with item details

---
+++ handlers/menu.handlers.php
@@ -0,0 +1,21 @@
+<?php
+$bestSellers = [
+    [
+        "name" => "adobo",
+        "price" => 149,
+        "desc" => "classic filipino dish made with soy sauce and vinegar.",
+        "img" => "adobo.jpg"
+    ],
+    [
+        "name" => "lechon",
+        "price" => 299,
+        "desc" => "crispy roasted pork with liver sauce.",
+        "img" => "lechon.jpg"
+    ],
+    [
+        "name" => "halo-halo",
+        "price" => 99,
+        "desc" => "mixed shaved ice dessert with sweet toppings.",
+        "img" => "halohalo.jpg"
+    ]
+];
