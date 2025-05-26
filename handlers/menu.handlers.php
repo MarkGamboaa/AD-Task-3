7552ce19d155f6a1b93de77ff371f4c8e7361f31
MarkGamboa <202310110@fit.edu.ph>
2025-05-26 17:52:55 +0800

    feat: Expand menu with additional Filipino dishes and update item details

--- handlers/menu.handlers.php
+++ handlers/menu.handlers.php
@@ -19,3 +19,78 @@ $bestSellers = [
         "img" => "halohalo.jpg"
     ]
 ];
+
+$menu = [
+    [
+        "name" => "adobo",
+        "price" => 149,
+        "desc" => "classic filipino dish made with soy sauce and vinegar.",
+        "img" => "adobo.jpg"
+    ],
+    [
+        "name" => "sinigang",
+        "price" => 159,
+        "desc" => "tamarind-based sour soup with pork and vegetables.",
+        "img" => "sinigang.jpg"
+    ],
+    [
+        "name" => "lechon",
+        "price" => 299,
+        "desc" => "crispy roasted pork with liver sauce.",
+        "img" => "lechon.jpg"
+    ],
+    [
+        "name" => "pancit bihon",
+        "price" => 129,
+        "desc" => "stir-fried rice noodles with vegetables and meat.",
+        "img" => "pancit.jpg"
+    ],
+    [
+        "name" => "kare-kare",
+        "price" => 189,
+        "desc" => "oxtail stew with peanut sauce, served with bagoong.",
+        "img" => "karekare.jpg"
+    ],
+    [
+        "name" => "halo-halo",
+        "price" => 99,
+        "desc" => "mixed shaved ice dessert with sweet toppings.",
+        "img" => "halohalo.jpg"
+    ],
+    [
+        "name" => "laing",
+        "price" => 109,
+        "desc" => "taro leaves in coconut milk with chili and dried fish.",
+        "img" => "laing.jpg"
+    ],
+    [
+        "name" => "bistek tagalog",
+        "price" => 169,
+        "desc" => "filipino beef steak marinated in soy sauce and calamansi.",
+        "img" => "bistek.jpg"
+    ],
+    [
+        "name" => "bulalo",
+        "price" => 239,
+        "desc" => "beef marrow stew with corn and vegetables.",
+        "img" => "bulalo.jpg"
+    ],
+    [
+        "name" => "tocino",
+        "price" => 129,
+        "desc" => "sweet cured pork served with garlic rice and egg.",
+        "img" => "tocino.jpg"
+    ],
+    [
+        "name" => "inihaw na liempo",
+        "price" => 179,
+        "desc" => "grilled pork belly marinated in spices.",
+        "img" => "liempo.jpg"
+    ],
+    [
+        "name" => "ginataang gulay",
+        "price" => 109,
+        "desc" => "vegetables in coconut milk with shrimp or dried fish.",
+        "img" => "ginataang.jpg"
+    ]
+];
