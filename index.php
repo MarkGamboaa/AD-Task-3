7b76e9cae10ecd9f86fee5f3d29f7c3a396351cd
MarkGamboa <202310110@fit.edu.ph>
2025-05-26 17:42:27 +0800

    feat: Initialize index, menu handler, and utils display menu file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require BASE_PATH . '/vendor/autoload.php';
+require BASE_PATH . '/utils/display.menu.php';
+require_once BASE_PATH . '/handlers/menu.handler.php';
+require_once BASE_PATH . '/components/templates/header.component.php';
+?>
+
+
+
+<?php require_once BASE_PATH . '/components/templates/footer.component.php'; ?>
